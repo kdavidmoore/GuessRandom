029af74a4471e677c35a6e0c37edfeacd9287eae
Keith Moore <keith.m4@gmail.com>
2016-02-28 09:36:36 -0500

    GuessRandom 2.0
    Replaced with linear search with a basic binary search algorithm.

--- guessRandom.py
+++ guessRandom.py
@@ -1,32 +1,25 @@
-# Instructions from: https://www.daniweb.com/programming/software-development/threads/131973/5-crucial-projects-for-beginners
-# Instructions: The computer randomly generates a number. The user inputs a number, and the computer will tell you if you are too high, or too low. Then you will get to keep guessing until you guess the number.
-# My solution is as follows...
+#Demonstration of binary search algorithm
+
 import random
 
-while True:
-    try:
-        print "Welcome to the randum number guessing game."
-        low = int(raw_input("Enter a lower bound (must be an integer) > "))
-        high = int(raw_input("Now enter an upper bound (must be an integer) > "))
-        break
-    except ValueError:
-        print "You did something wrong..."
-      
-rand = random.randint(low,high)
-guess = rand + 1
+maxGuess = 100
+minGuess = 1
+correctNum = random.randint(1,100)
 
-while True:
-    try:
-        while rand != guess:
-            guess = int(raw_input("Make your guess > "))
-            if guess > rand:
-                print "Your guess is too high. Try again.\n"
-            elif guess < rand:
-                print "Your guess is too low. Try again.\n"
-            elif guess == rand:
-                print "You got it right!"
-            else:
-                print "Something went wrong..."
-        break
-    except ValueError:
-        print "That's not an integer..."
+print "Guess a whole number between 1 and 100"
+
+myGuess = int(raw_input("> "))
+
+while myGuess != correctNum:
+	if myGuess > correctNum:
+		maxGuess = myGuess - 1
+		print "Your guess was too high."
+		print "Guess a new number between " + str(minGuess) + " and " + str(maxGuess)
+		myGuess = int(raw_input("> "))
+	else:
+		minGuess = myGuess + 1
+		print "Your guess was too low."
+		print "Guess a new number between " + str(minGuess) + " and " + str(maxGuess)
+		myGuess = int(raw_input("> "))
+
+print "That's it. The correct number was " + str(correctNum)
